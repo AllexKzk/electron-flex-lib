c6099c15cbb26d50d2a5ffe21b0d924aa3727155
AllexKzk <san4a_kozikov@mail.ru>
2023-05-26 02:34:08 +0300

    sliding btns in page

--- src/View/Page/Overlay.jsx
+++ src/View/Page/Overlay.jsx
@@ -2,33 +2,60 @@ import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import SaveIcon from '@mui/icons-material/Save';
-import {Box, IconButton} from "@mui/material";
+import {IconButton} from "@mui/material";
 import {themeContext} from "../Theming/themes";
-import {useContext} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import {useNavigate} from "react-router-dom";
 
+
+//all startup's money went to sliding buttons:
 export default function Overlay(props) {
     const theme = useContext(themeContext);
+    const [scrollYPos, setScrollYPos] = useState(0);
+    const blockRef = useRef();
+
+    useEffect(() => {
+        document.addEventListener('scroll', handleScroll);
+        return () => {
+            document.removeEventListener('scroll', handleScroll);
+        };
+    });
+
+    const handleScroll = (e) => {
+        const yPos = window.pageYOffset;
+        if (yPos >= scrollYPos){    //hide if scroll down
+            blockRef.current.style.top = '-50px';
+        }
+        if (!yPos)                  //show on top
+            blockRef.current.style.top = '0';
+        setScrollYPos(yPos);
+    };
+
     const navigate = useNavigate();
     const backToFolder = () => {
-        navigate('/', {
+        navigate('/', {         //return to explorer
             state: {path: props.path}
         });
     };
-
+    
     return (
-        <Box style={{display: 'flex', justifyContent: 'space-between', m: '0 5vw 0 5vw'}}>
-           <IconButton size={'large'} onClick={backToFolder}>
-               <ArrowBackIosIcon />
-           </IconButton>
-            <Box>
-                <IconButton size={'large'} onClick={theme.changeTheme}>
-                    {theme.theme.palette.mode === 'dark' ? <Brightness4Icon /> : <Brightness7Icon />}
-                </IconButton>
-                <IconButton size={'large'} onClick={() => window.electron.openSource(props.source)}>
-                    <SaveIcon/>
-                </IconButton>
-            </Box>
-        </Box>
+        <div 
+            onMouseEnter={() => {blockRef.current.style.top = 0}} 
+            onMouseLeave={() => {blockRef.current.style.top = scrollYPos ? '-50px' : '0'}}
+            style={{position: 'fixed', width: '100%', height: '50px'}}>
+                <div ref={blockRef} style={{top: 0, position: 'fixed', width: '100%', transitionDuration: '500ms', display: 'flex', justifyContent: 'space-between'}}>
+                    <IconButton size={'large'} onClick={backToFolder}>
+                        <ArrowBackIosIcon />
+                    </IconButton>
+                    <div>
+                        <IconButton size={'large'} onClick={theme.changeTheme}>
+                            {theme.theme.palette.mode === 'dark' ? <Brightness4Icon /> : <Brightness7Icon />}
+                        </IconButton>
+                        <IconButton size={'large'} onClick={() => window.electron.openSource(props.source)}>
+                            <SaveIcon/>
+                        </IconButton>
+                    </div>
+                </div>
+        </div>
     );
 }
